117190ebed6111db793da45d33b616cfd11a0725
hanbin <hnbn707@gmail.com>
2022-03-24 16:40:56 +0900

    fix : write 라우터, 컨트롤러 분리

--- back/routes/board/index.js
+++ back/routes/board/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const noticeRouter = require('./notice')
 const qnaRouter = require('./qna')
 const mainRouter = require('./main')
-const { promisePool } = require('../../db')
+const boardCon = require('./boardController')
 
 const router = express.Router();
 
@@ -10,24 +10,7 @@ router.use('/notice', noticeRouter);
 router.use('/qna', qnaRouter);
 router.use('/main', mainRouter);
 
-router.post('/writeCategory', async (req, res) => {
-    const { currentBoard } = req.body
-    const sql = `select * from category where board_name = '${currentBoard}'`
-    let response = {
-        errno: 1
-    }
-    try {
-        const [result] = await promisePool.execute(sql)
-        response = {
-            ...response,
-            errno: 0,
-            result: result,
-        }
-        res.json(response)
-    } catch (e) {
-        console.log(e)
-        res.json(response)
-    }
-})
+router.post('/writeCategory', boardCon.writeCategory)
+router.post('/write', boardCon.writePost)
 
 module.exports = router;
