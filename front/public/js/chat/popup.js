f1c779fe679fc7016fb48474cef88112253f102d
bitkunst <bitkunst23@gmail.com>
2022-03-25 03:35:01 +0900

    update: chatting logic / add chatting css

--- front/public/js/chat/popup.js
+++ front/public/js/chat/popup.js
@@ -1,10 +1,10 @@
 
 function openWindowPop(url, name){
-    const popupWidth = 500
-    const popupHeight = 600
+    const popupWidth = 600
+    const popupHeight = 700
     const popupX = (window.screen.width/2) - (popupWidth/2)
     const popupY = (window.screen.height/2) - (popupHeight/2)
-    var options = `top=${popupY}, left=${popupX} width=${popupWidth}, height=${popupHeight}, status=no, menubar=no, toolbar=no, resizable=no`
+    const options = `top=${popupY}, left=${popupX} width=${popupWidth}, height=${popupHeight}, status=no, menubar=no, toolbar=no, resizable=no, location=no, scrollbars=no`
     window.open(url, name, options)
 }
 
--- front/routes/chat/chatController.js
+++ front/routes/chat/chatController.js
@@ -1,15 +1,19 @@
 const axios = require('axios')
 
 const getChatList = async (req, res) => {
-
-    const response = await axios.get('http://localhost:4001/api/chat/list', {
-        withCredentials: true
-    })
-    const result = response.data.rows
-
-    res.render('./chat/chat.html',{
-        result
-    })
+    try {
+        const response = await axios.get('http://localhost:4001/api/chat/list', {
+            withCredentials: true
+        })
+        const result = response.data.rows
+    
+        res.render('./chat/chat.html',{
+            result
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Internal Server Error')
+    }
 }
 
 const getChatRoom = (req, res) => {
