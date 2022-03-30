b75b351571900e970d619ed97963b46664b08b23
hanbin <hnbn707@gmail.com>
2022-03-30 14:51:55 +0900

    feat : comment CRUD

--- back/routes/comment/commentController.js
+++ back/routes/comment/commentController.js
@@ -1,88 +1,94 @@
-const pool = require('../../db').pool
+const { promisePool } = require('../../db')
+const { decodePayload } = require('../../utils/jwt')
 
 // 댓글 모두 ajax
 // 댓글 리스트 브라우저로 전달
 const viewApi = async (req, res) => {
-    const sql = ``
+    const { bid } = req.body
+    const token = req.cookies.AccessToken
+    const userinfo = decodePayload(token)
     let response = {
         errno: 1
     }
-    const conn = await pool.getConnection()
     try {
-        const [result] = await conn.execute(sql)
+        let sql1 = `SELECT cid, comment, DATE_FORMAT(c_date,'%Y-%m-%d') as c_date, bid, c_userid from comment where bid = ${bid};`
+        const [result] = await promisePool.execute(sql1)
         response = {
             ...response,
             errno: 0,
-            result: result,
+            result
         }
         res.json(response)
     } catch (e) {
         console.log(e)
-        res.json(response)
-    } finally { conn.release() }
+    }
 }
 
 // 새 댓글 작성 시 db에 추가
 const writeApi = async (req, res) => {
-    const sql = ``
+    const { bid, comment } = req.body
+    const token = req.cookies.AccessToken
+    const userinfo = decodePayload(token)
     let response = {
         errno: 1
     }
-    const conn = await pool.getConnection()
     try {
-        const [result] = await conn.execute(sql)
+        let sql1 = `
+        INSERT INTO comment(bid, c_date, comment, c_userid) 
+        values(${bid},now(), '${comment}', '${userinfo.userid}') ;`
+        const [result] = await promisePool.execute(sql1)
         response = {
             ...response,
             errno: 0,
-            result: result,
+            result
         }
         res.json(response)
     } catch (e) {
         console.log(e)
-        res.json(response)
-    } finally { conn.release() }
+    }
 }
 
 // update로 수정한 내용 db에 저장
 const updateApi = async (req, res) => {
-    const sql = ``
+    const { cid, cngCom } = req.body
+    const token = req.cookies.AccessToken
+    const userinfo = decodePayload(token)
     let response = {
         errno: 1
     }
-    const conn = await pool.getConnection()
     try {
-        const [result] = await conn.execute(sql)
+        console.log('업데이트실행')
+        let sql1 = `UPDATE comment SET comment='${cngCom}' where cid=${cid};`
+        const [result] = await promisePool.execute(sql1)
         response = {
             ...response,
             errno: 0,
-            result: result,
         }
         res.json(response)
     } catch (e) {
         console.log(e)
-        res.json(response)
-    } finally { conn.release() }
+    }
 }
 
 // delete시 삭제권한 확인 후 db에서 해당 내용 삭제
 const deleteApi = async (req, res) => {
-    const sql = ``
+    const { cid } = req.body
+    const token = req.cookies.AccessToken
+    const userinfo = decodePayload(token)
     let response = {
         errno: 1
     }
-    const conn = await pool.getConnection()
     try {
-        const [result] = await conn.execute(sql)
+        let sql1 = `DELETE FROM comment where cid = ${cid};`
+        const [result] = await promisePool.execute(sql1)
         response = {
             ...response,
             errno: 0,
-            result: result,
         }
         res.json(response)
     } catch (e) {
         console.log(e)
-        res.json(response)
-    } finally { conn.release() }
+    }
 }
 
 // 댓글 좋아요 기능은 시간 되면 넣기..
--- front/public/js/board/viewAjax.js
+++ front/public/js/board/viewAjax.js
@@ -107,4 +107,164 @@ scrapBtn.addEventListener('submit', async (e) => {
     e.preventDefault()
     await ajaxScrap('http://localhost:4001/api/board/main/scrap')
 })
+
+
+
 // 3. comment ajax
+// 3-1. 댓글 인풋 클릭 시 댓글 submit 버튼 생성, 취소 누르면 사라짐
+const commentForm = document.querySelector('#commentForm')
+const commentInput = document.querySelector('#commentInput')
+const c_userid = document.querySelector('#c_userid')
+const comBtnDiv = document.querySelector('#comBtnDiv')
+const cancelBtn = document.querySelector('#cancelBtn')
+const commentBtn = document.querySelector('#commentBtn')
+
+commentInput.addEventListener('focus', () => {
+    comBtnDiv.style.display = 'block'
+    cancelBtn.addEventListener('click', () => {
+        commentInput.value = ''
+        comBtnDiv.style.display = 'none'
+    })
+})
+
+// 3-2. 댓글 작성 후 db에 저장 : write
+commentBtn.addEventListener('click', async (e) => {
+    try {
+        const router = 'http://localhost:4001/api/comment/write'
+        const option = {
+            'Content-type': 'application/json',
+            withCredentials: true
+        }
+        const data = {
+            bid: idx,
+            comment: commentInput.value,
+            c_userid: c_userid.value
+        }
+        const response = await axios.post(router, data, option)
+        const errNo = response.data.errno
+        if (errNo === 0) {
+            commentInput.value = ''
+            viewComment()
+        } else {
+            alert(response.data.errMsg)
+        }
+    }
+    catch (e) {
+        console.log(`axios 통신 중 에러발생 : ${e.message}`)
+        alert('axios 통신 중 문제가 발생했습니다')
+    }
+})
+
+// 3-3. 댓글 불러오기 : view
+viewComment()
+async function viewComment() {
+    try {
+        const router = 'http://localhost:4001/api/comment/view'
+        const option = {
+            'Content-type': 'application/json',
+            withCredentials: true
+        }
+        const data = {
+            bid: idx,
+        }
+        const response = await axios.post(router, data, option)
+        if (response.data.errno === 0) {
+            const commentArr = response.data.result
+            renderComment(commentArr)
+        }
+        else {
+            alert('db에러발생')
+        }
+    }
+    catch (e) {
+        console.log(`axios 통신 중 에러발생 : ${e.message}`)
+        alert('axios 통신 중 문제가 발생했습니다')
+    }
+}
+
+// 3-3-1. 댓글 리스트 화면에 랜더링 해주는 함수 : view 서브함수
+function renderComment(Arr) {
+    const commentUl = document.querySelector('#commentUl')
+    const c_template = document.querySelector('#commentTemplate').innerHTML
+    const commentCount = document.querySelector('#commentCount')
+    let str = ''
+    Arr.forEach(v => {
+        str += c_template
+            .replace(/{cid}/gi, v.cid)
+            .replace(/{comment}/gi, v.comment)
+            .replace(/{c_date}/gi, v.c_date)
+            .replace(/{c_userid}/gi, v.c_userid)
+            .replace(/{parent}/gi, v.parent)
+    })
+    commentUl.innerHTML = str
+    commentCount.innerHTML = `댓글 ${Arr.length}개`
+
+    // 댓글삭제 addEvent
+    const commentLi = document.querySelectorAll('.commentLi')
+    commentLi.forEach(v => {
+        v.querySelector('.comUdtBtn').addEventListener('click', updateHandler)
+        v.querySelector('.comDelBtn').addEventListener('click', deleteHandler)
+    })
+}
+
+// 3-4. 댓글 삭제
+async function deleteHandler(e) {
+    const cid = e.target.parentNode.querySelector('input').value
+    try {
+        const router = 'http://localhost:4001/api/comment/delete'
+        const option = {
+            'Content-type': 'application/json',
+            withCredentials: true
+        }
+        const data = { cid }
+
+        const response = await axios.post(router, data, option)
+
+        if (response.data.errno === 0) {
+            viewComment()
+        }
+        else {
+            alert('db에러발생')
+        }
+    }
+    catch (e) {
+        console.log(`axios 통신 중 에러발생 : ${e.message}`)
+        alert('axios 통신 중 문제가 발생했습니다')
+    }
+}
+
+
+// 3-4. 댓글 수정
+async function updateHandler(e) {
+    const commentContent = e.target.parentNode.parentNode.querySelector('.commentContent')
+    const cid = e.target.parentNode.querySelector('input').value
+    const originalComment = commentContent.innerHTML
+    commentContent.innerHTML = `<input type="text" class="cngCom" value="${originalComment}"><button class="cngBtn">수정</button>`
+    const cngBtn = commentContent.querySelector('.cngBtn')
+    cngBtn.addEventListener('click', updateAxios)
+
+    async function updateAxios() {
+        try {
+            let cngCom = commentContent.querySelector('.cngCom').value
+            const router = 'http://localhost:4001/api/comment/update'
+            const option = {
+                'Content-type': 'application/json',
+                withCredentials: true
+            }
+            const data = { cid, cngCom }
+
+            const response = await axios.post(router, data, option)
+
+            if (response.data.errno === 0) {
+                viewComment()
+            }
+            else {
+                alert('db에러발생')
+            }
+        }
+        catch (e) {
+            console.log(`axios 통신 중 에러발생 : ${e.message}`)
+            alert('axios 통신 중 문제가 발생했습니다')
+        }
+    }
+}
