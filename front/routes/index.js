92079107117079bfc9f4f6533636b4e3eb88bc05
jongs <jongnam0309@gmail.com>
2022-04-01 17:07:29 +0900

    feat :Edit profile , logout / fix : user/join issue solved

--- front/routes/index.js
+++ front/routes/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const { loginAuth } = require('../middleware/loginAuth.js')
 const userRouter = require('./user/userRouter.js')
 const boardRouter = require('./board')
 const chatRouter = require('./chat')
 const searchRouter = require('./search')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('index.html')
+
+router.get('/', loginAuth, (req, res) => {
+    const userInfo = req.userInfo
+    res.render('index.html', {
+        userInfo
+    })
 })
 
 router.use('/user', userRouter)
--- front/routes/user/userController.js
+++ front/routes/user/userController.js
@@ -1,8 +1,69 @@
 const { decodePayload } = require('../../utils/jwt.js')
+const client_id = '50b75fc17d47bf2ba9978434d0a940cf'
+const redirect_uri = 'http://localhost:3001/oauth/kakao'
+const client_secret = 'CFOkTpjElWk2vT1I4g4nb7xrCjhHyu3K'
+const host = 'https://kauth.kakao.com'
+const qs = require('qs')
+const { default: axios } = require('axios')
+const { alertmove } = require('../../utils/alertmove.js');
+
+
 
 exports.login = (req, res) => {
     res.render('user/login.html');
 };
+
+
+exports.logout = (req, res) => {
+    res.clearCookie('AccessToken');
+    res.send(alertmove('http://localhost:3001', '로그아웃 되었습니다.'));
+}
+
+exports.kakaoLogin = (req, res) => {
+    const redirectURI = host + `/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`
+    res.redirect(redirectURI)
+};
+
+exports.oauthkakao = async (req, res) => {
+    const { query: { code } } = req
+    const token_url = host + '/oauth/token'
+    const headers = {
+        'Content-type': 'application/x-www-form-urlencoded'
+    }
+
+    const body = qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: '50b75fc17d47bf2ba9978434d0a940cf',
+        redirect_uri: 'http://localhost:3001/oauth/kakao',
+        code: code,
+        client_secret,
+    })
+
+
+
+    // 2. 토큰받기
+
+    const response = await axios.post(token_url, body, headers)
+    response.data.access_token
+
+    // 3. 토큰을 활용하여 사용자 정보 가져오기
+    try {
+        const { access_token: ACCESS_TOKEN } = response.data
+        const url = 'https://kapi.kakao.com/v2/user/me'
+        const userinfo = await axios.post(url, null, {
+            headers: {
+                'Authorization': `Bearer ${ACCESS_TOKEN}`
+            }
+        })
+        console.log(userinfo.data.kakao_account.profile.profile_image_url)
+        console.log(userinfo.data.kakao_account.profile.nickname)
+
+
+    } catch (e) {
+        console.log(e)
+    }
+    res.send('로그인성공')
+}
 
 exports.join = (req, res) => {
     res.render('user/join.html');
@@ -20,15 +81,78 @@ exports.profile = async (req, res) => {
     // To 종남
     // req 객체 안에 사용자 정보 넣어서 userInfo 객체 만듦!!
     // 확인하고 작업할 것!! 
-    console.log(req.userInfo)
+    // console.log(req.userInfo)
     // 백서버로 요청을 보내고 사용자 정보를 가져오기 
     // const response = awiat axios.post()
-    // res.render('user/profile.html', {
-    //     userid,
-    //     nickname
-    // });
+    const option = {
+        'Content-type': 'application/json',
+        withCredentials: true,
+    }
+    const data = { // 백으로 보내는 용도
+        userid: req.userInfo.userid
+    }
+    const response = await axios.post('http://localhost:4001/api/user/profile', data, option)
+    res.render('user/profile.html', {
+        user: response.data
+    });
 };
 
-exports.profileEdit = (req, res) => {
-    res.render('user/update.html');
+exports.profileEdit = async (req, res) => {
+    const option = {
+        'Content-type': 'application/json',
+        withCredentials: true,
+    }
+    const data = { // 백으로 보내는 용도
+        userid: req.userInfo.userid
+    }
+    const response = await axios.post('http://localhost:4001/api/user/profile', data, option)
+
+    let result = {
+        ...response.data
+    }
+    const mobileArr = result.mobile.split('-')
+    const mobile1 = mobileArr[0]
+    const mobile2 = mobileArr[1]
+    const mobile3 = mobileArr[2]
+    delete result.mobile
+    result = {
+        ...result,
+        mobile1,
+        mobile2,
+        mobile3
+    }
+
+    let phoneArr, phone1, phone2, phone3
+    if (result.phone) {
+        phoneArr = response.data.phone.split('-')
+        phone1 = phoneArr[0]
+        phone2 = phoneArr[1]
+        phone3 = phoneArr[2]
+        result = {
+            ...result,
+            phone1,
+            phone2,
+            phone3
+        }
+    }
+
+
+    res.render('user/profileUpdate.html', {
+        user: result //넌적스
+    });
+};
+
+exports.nickchk = async (req, res) => {
+    const { nickname } = req.body;
+
+    const sql = `SELECT * FROM user WHERE nickname = "${nickname}"`
+    let [result] = await promisePool.query(sql)
+
+    if (nickname == '') {
+        res.send({ er: 0 })
+    } else if (result.length == 1) {
+        res.send({ er: 1 })
+    } else {
+        res.send({ er: 2 })
+    }
 };
