2ab3a650260a2f6e8ea71b3bf95385f49e60b60b
hanbin <hnbn707@gmail.com>
2022-04-08 12:04:03 +0900

    fix : 버그수정

--- front/routes/board/qna/index.js
+++ front/routes/board/qna/index.js
@@ -48,8 +48,22 @@ router.get('/write', auth, (req, res) => {
 })
 
 // ajax로 게시판/카테고리 구성하기 or  그냥 html 분리하기, nunjucks로 {item} 넣어주기, back서버와 통신 필요
-router.get('/update', auth, (req, res) => {
-    res.render('board/update')
+router.get('/update/:idx', auth, async (req, res) => {
+    try {
+        const idx = req.params.idx
+        const router = 'http://localhost:4001/api/board/getArticle'
+        const option = {
+            'Content-type': 'application/json',
+            withCredentials: true
+        }
+        const data = { idx }
+        const response = await axios.post(router, data, option)
+        const item = response.data.result[0]
+        res.render('board/update', { item })
+    } catch (error) {
+        console.log(error)
+        res.send('axios 오류')
+    }
 })
 
 module.exports = router
